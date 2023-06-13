eb478e3bdc4629edca3ac9b673b3ccb0076c38ba
MarceloAssis123 <124265415+MarceloAssis123@users.noreply.github.com>
2023-06-13 11:15:52 -0300

    GitPages

---
+++ docs/ajax.js
@@ -0,0 +1,12 @@
+$('#buttonCurso').click(() => {
+    $.ajax({
+        url: './curso.txt',
+        type: 'GET',
+        dataType: 'text',
+        async: true,
+        success: (data) => {
+
+            $('#labelCurso').html(' | ' + data);
+        }
+    })
+})
